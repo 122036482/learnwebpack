93443e16963e0ee00e54a79ff6c0d1f07250d1b3
maojx123 <122036482@qq.com>
2021-05-08 17:10:19 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import _ from "lodash";
-// 引入class
-import "./style.css";
-// 引入图片
-import Icon from "./ico.png";
-// 引入数据格式
-import Data from "./data.xml";
-import Notes from "./data.csv";
+// 引入函数
+import printMe from "./print.js";
 function component() {
   const element = document.createElement("div");
+  // 创建一个按钮
+  const btn = document.createElement("button");
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  //   添加一个样式的类
-  element.classList.add("hello");
-  //   创建一个img标签
-  const myIcon = new Image();
-  //   添加src的属性 赋值为  Icon
-  myIcon.src = Icon;
-  //   添加到div 元素内
-  element.appendChild(myIcon);
 
-  // 在控制台输出
-  console.log(Data);
-  console.log(Notes);
+  // 给按钮添加内容
+  btn.innerHTML = "Click me and check the console!";
+  // 绑定事件
+  btn.onclick = printMe;
+  // 天加到 element 元素内
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,23 @@
 const path = require("path");
+// 添加插件
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  // 入口
-  entry: "./src/index.js", // 路径
-  //   输出
-  output: {
-    //   输出的文件名
-    // filename: "main.js",
-    filename: "boundle.js",
-
-    // 输出的路径
-    path: path.resolve(__dirname, "dist")
+  // 创建多个入口
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js"
   },
-  //   添加loader
-  module: {
-    //   规则
-    rules: [
-      // 数组是添加多个规则
-      //   处理css
-      {
-        test: /\.css$/i, // 匹配对应的文件后最
-        use: ["style-loader", "css-loader"] // 使用对应的loader
-      },
-      //   处理图片
-      {
-        test: /\.(png|svg|jpg|gif)$/i,
-        type: "asset/resource" // webpack5 中使用的是 内置的 Asset Modules
-      },
-      // 处理字体
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource"
-      },
-      // 加载数据
-      // csv
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"]
-      },
-      // 加载数据
-      // csv
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"]
-      }
-    ]
-  }
+  output: {
+    // 输出的文件名 [name]: entry的key + .boundle.js
+    filename: "[name].boundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "输出管理"
+    })
+  ]
 };
