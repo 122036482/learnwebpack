a2a77a2eccf0d05a5d8c1fe20273677e6f41770f
maojx123 <122036482@qq.com>
2021-05-08 17:42:39 +0800

    开发设置静态服务 更新文件

--- webpack.config.js
+++ webpack.config.js
@@ -13,11 +13,17 @@ module.exports = {
     // 输出的文件名 [name]: entry的key + .boundle.js
     filename: "[name].boundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true
+    clean: true,
+    publicPath: "/"
   },
+  // 开个服务器 需要安装 webpack-dev-server
+  devServer: {
+    contentBase: "./dist"
+  },
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      title: "输出管理"
+      title: "Development"
     })
   ]
 };
